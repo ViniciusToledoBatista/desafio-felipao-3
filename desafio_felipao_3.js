4e56311afe04703801a7ab9fef5821e1187dbd16
Vinicius Toledo <vinitoledob@icloud.com>
2024-10-18 15:58:58 -0300

    Adicionando o codigo

--- desafio_felipao_3.js
+++ desafio_felipao_3.js
@@ -1,1 +1,39 @@
-console.log ("Hello, friend!")
+class heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome
+      this.idade = idade
+      this.tipo = tipo
+    }
+  
+    atacar() {
+      let ataque
+  
+      switch (this.tipo) {
+        case "mago":
+          ataque = "magia"
+          break
+        case "guerreiro":
+          ataque = "espada"
+          break
+        case "monge":
+          ataque = "artes marciais"
+          break
+        case "ninja":
+          ataque = "shuriken"
+          break
+      }
+  
+      console.log(`O Herói ${this.tipo} ${this.nome} atacou usando ${ataque}.`)
+      
+    }
+  }
+  
+  const heroi1 = new heroi("Merlin", 300, "mago")
+  const heroi2 = new heroi("Kratos", 80, "guerreiro")
+  const heroi3 = new heroi("Dhalsim", 200, "monge")
+  const heroi4 = new heroi("Kenshin Himura", 40, "ninja")
+  
+  heroi1.atacar ()
+  heroi2.atacar ()
+  heroi3.atacar ()
+  heroi4.atacar ()
